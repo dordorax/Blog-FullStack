ecfd1d733fd7daea95c064d4d3c0b00534e0bc00
dordorax <dordorax.dh@gmail.com>
2021-04-08 19:42:26 +0430

    update order file

--- ecommerce-backend/api/order/controllers/order.js
+++ ecommerce-backend/api/order/controllers/order.js
@@ -1,53 +1,64 @@
 'use strict';
 
-const { parseMultipartData, sanitizeEntity } = require('strapi-utils'); 
 
 module.exports = {
 
     async create(ctx) {
 
-        const db_products = await strapi.query('products').find({products});
+        console.log('###########################')
+        console.log(ctx.request.body.product);
+        console.log(ctx.state.user.balance);
+        console.log(ctx.state.user.id);
+        console.log('###########################')
+     
+        const {
+            city,
+            address,
+            postal_code,
+            amount,
+        } = ctx.request.body;
 
-        let entity;
-        if (ctx.is('multipart')) {
+        const product = await strapi.query('Product').findOne({ id: ctx.request.body.product.id});
+        console.log(product.quantity);
+        const wallet = ctx.state.user.balance;
+        const total_price = product.price * amount;
 
-            const {
-                city,
-                address,
-                postal_code,
-                amount,
-            } = parseMultipartData(ctx);
-        
-            entity = await strapi.services.order.create({
-                user: ctx.state.user.id,
-                city,
-                address,
-                postal_code,
-                amount,
-                products: db_products,
-                total_price: db_products.price * amount,
-            });
-        } 
-        else {
+        if(product != null && wallet <= total_price){
+            try{
+                const order = await strapi.query('Order').create({
+                    city,
+                    address,
+                    postal_code,
+                    amount,
+                    total_price,
+                    product,
+                    user: ctx.state.user.id
+                })
 
-        entity = await strapi.services.order.create(ctx.request.body);
-
-        } 
-
-        return sanitizeEntity(entity, { model: strapi.models.order });
+                await starpi.query('User').update({id: ctx.state.user.id},{balance: ctx.state.user.balance - total_price});
+                await strapi.query('Product').update({id: product.id},{quantity: ctx.product.quantity - amount});
+                
+                console.log('***************************')
+                console.log(ctx.state.user.balance);
+                console.log(ctx.product.quantity);
+                console.log(order);                
+                return {
+                    'status': 200,
+                    'message': `New order #${order.id} created successfully!`
+                };        
+    
+            }
+            catch(err) {
+                console.log(err)
+                return {
+                    'status': 400,
+                    'message': "Failed to create order"
+                };
+            }
+        }
     },
 
     async update(ctx) {
-        const { id } = ctx.params;
-    
-        let entity;
-        if (ctx.is('multipart')) {
-          const { status } = parseMultipartData(ctx);
-          entity = await strapi.services.order.update({ id }, status);
-        } else {
-          entity = await strapi.services.order.update({ id }, ctx.request.body);
-        }
-    
-        return sanitizeEntity(entity, { model: strapi.models.order });
+        return strapi.query('Order').update({id: ctx.params.id},{status: ctx.request.body.status});
     }
 };
